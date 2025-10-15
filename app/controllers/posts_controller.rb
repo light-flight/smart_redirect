337f46c7a069f7be90e826a9408d1024269d0462
Georgy Yuriev <georgy.yuriev@gmail.com>
2025-10-15 11:57:11 +0300

    refactor action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,15 +36,8 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post.update(post_params)
+    redirect_to @post, notice: "Post was successfully updated."
   end
 
   # DELETE /posts/1 or /posts/1.json
